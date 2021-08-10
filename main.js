62265a880cd8b1c21e56fc6319bf00798334c0e1
18taieb29 <belaid.taieb@gmail.com>
2021-08-10 12:23:56 +0200

    first changes

--- main.js
+++ main.js
@@ -1,1 +1,57 @@
-$('div')
+// q = question
+// o = option
+// a = answer
+DataQuiz = [
+    {
+        q : "What is the standard distance between the target and archer in Olympics?",
+        o : [
+          "50 meters",
+          "70 meters",
+          "100 meters",
+          "120 meters"
+        ],
+        a : 1 
+      },
+      {
+        q : "Which is the highest number on a standard roulette wheel?",
+        o : [
+          "22",
+          "24",
+          "32",
+          "36"
+        ],
+        a : 3
+      },
+      {
+        q : "How much wood could a woodchuck chuck if a woodchuck would chuck wood?",
+        o : [
+          "400 pounds",
+          "550 pounds",
+          "700 pounds",
+          "750 pounds"
+        ],
+        a : 2
+      },
+      {
+        q : "Which is the seventh planet from the sun?",
+        o : [
+          "Uranus",
+          "Earth",
+          "Pluto",
+          "Mars"
+        ],
+        a : 0
+      },
+      {
+        q : "Which is the largest ocean on Earth?",
+        o : [
+          "Atlantic Ocean",
+          "Indian Ocean",
+          "Arctic Ocean",
+          "Pacific Ocean"
+        ],
+        a : 3
+      }
+]
+
+
